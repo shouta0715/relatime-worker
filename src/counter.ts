62e0e8c278bfb54050993f001f77d766688f9998
shouta0715 <Myheroes0715@gmail.com>
2024-05-08 22:20:52 +0900

    add: basic durable objects

---
+++ src/counter.ts
@@ -0,0 +1,22 @@
+import { DurableObject } from 'cloudflare:workers';
+
+export class Counter extends DurableObject {
+  async getCounterValue() {
+    let value = (await this.ctx.storage.get('value')) || 0;
+    return value;
+  }
+
+  async increment(amount = 1) {
+    let value: number = (await this.ctx.storage.get('value')) || 0;
+    value += amount;
+    await this.ctx.storage.put('value', value);
+    return value;
+  }
+
+  async decrement(amount = 1) {
+    let value: number = (await this.ctx.storage.get('value')) || 0;
+    value -= amount;
+    await this.ctx.storage.put('value', value);
+    return value;
+  }
+}
